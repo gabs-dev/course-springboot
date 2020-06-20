276aef604c5d87244db254ef6930286f682414b7
gabs-dev <gabrielfelipe1458@gmail.com>
2020-06-19 21:17:41 -0300

    Category

---
+++ src/main/java/com/gabriel/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabriel.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
